cd871a9b8671300dbea471a303bed824c0c349f8
Erick Coutinho <erickcoutinho08@hotmail.com>
2022-10-03 20:10:41 -0300

    ex 1

--- first.py
+++ first.py
@@ -1,2 +1,9 @@
-nome = input('digite seu nome ')
-print(f"Olá {nome}, bem vindo a disciplina de programação. Parabéns pelo seu primeiro hello world, {nome}")
+#nome = input('digite seu nome ')
+#print(f"Olá {nome}, bem vindo a disciplina de programação. Parabéns pelo seu primeiro hello world, {nome}")
+a = 4
+b = 0.5
+c = 1
+x = input("digite o valor de x: ")
+x = float(x)
+y = a * x ** 2 + b * x + c
+print(f"O resultado de y para x = {x} é {y}.")
