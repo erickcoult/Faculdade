f730783a261068a9a2d91fd503cc19a948dad01b
Erick Coutinho <erickcoutinho08@hotmail.com>
2022-10-04 21:08:04 -0300

    Operadores

--- first.py
+++ first.py
@@ -1,9 +1,39 @@
+#atividade 1-------------
 #nome = input('digite seu nome ')
 #print(f"Olá {nome}, bem vindo a disciplina de programação. Parabéns pelo seu primeiro hello world, {nome}")
-a = 4
-b = 0.5
-c = 1
-x = input("digite o valor de x: ")
-x = float(x)
-y = a * x ** 2 + b * x + c
-print(f"O resultado de y para x = {x} é {y}.")
+
+#atividade 2-------------
+#a = 4
+#b = 0.5
+#c = 1
+#x = input("digite o valor de x: ")
+#x = float(x)
+#y = a * x ** 2 + b * x + c
+#print(f"O resultado de y para x = {x} é {y}.")
+
+#atividade 3--------------
+#a = input('valor 1: ')
+#b = input('valor 2: ')
+#if a < b:
+#    print('B é maior que A')
+#else:
+#    print("A é maior do que B")
+
+#a = 5
+#b = 10
+#if a < b:
+# print("a é menor do que b")
+#r = a + b
+#print(r)
+
+#atividade 4----------------
+#codigo_compra = 5222
+
+#if codigo_compra == 5222:
+    #print("Compra à vista.")
+#elif codigo_compra == 5333:
+    #print("Compra à prazo no boleto.")
+#elif codigo_compra == 5444:
+    #print("Compra à prazo no cartão.")
+#else:
+    #print("Código não cadastrado")
