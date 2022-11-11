6493ced4d85c95cc52ec478f807ed691db322481
Erick Coutinho <erickcoutinho08@hotmail.com>
2022-11-11 11:49:26 -0300

    language

--- APE/linguagemC.c
+++ APE/linguagemC.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+int main(int argc, char** argv) {
+    
+    char nome[30];
+    char endereco[30];
+    int telefone [15];
+    
+    printf("digite seu nome: \n");
+    fgets(nome, 30, stdin);
+    
+    printf("digite seu endereco\n");
+    fgets(endereco, 30, stdin);
+    
+    printf("digite telefone \n");
+    fgets(telefone, 15, stdin);
+    
+    printf("\n Nome: %s",nome );
+    
+    printf("\n Endereco: %s", endereco );
+    
+    printf("\n Telefone: %s", telefone );
 
-int main(int argc, char** argv)
-{
-    int valor = 12;
-    printf("o valor de int %e", valor);
     return (EXIT_SUCCESS);
 }
+ 
